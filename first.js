578cfb1135622a7ee7f21c7ff172784bae0e822f
RabiaHameedKhan <rabiahameedkhan27@gmail.com>
2024-08-11 15:54:40 +0500

    message

--- first.js
+++ first.js
@@ -61,11 +61,89 @@ console.log("Javascript learning Day 1");
 
 //TASK 4: ARRAYS
 
-let marks=[10, 20, 30, 40,50];
+// let marks=[10, 20, 30, 40,50];
 
-for(i=1; i<marks.length; i++){
-    let offers= marks[i]/10;
-    marks[i]-=offers
-}
-console.log(marks);
+// for(i=1; i<marks.length; i++){
+//     let offers= marks[i]/10;
+//     marks[i]-=offers
+// }
+// console.log(marks);
+
+
+//PUSH AND POP
+
+// let chocolates=["kitkat", "cadbury", "bounty", "Mars"];
+// console.log(chocolates);
+
+// chocolates.push("You");
+// console.log(chocolates);
+
+// deleted= chocolates.pop();
+//  console.log(chocolates);
+
+//  console.log("deleted chocolate:", deleted);
+
+//  //TOSTRING
+
+//  console.log(chocolates.toString());
+
+//  //CONCAT
+
+//  let coffee=["latte", "iced coffee", "mocha"];
+
+//  let fav=coffee.concat(chocolates);
+//  console.log(fav);
+//  console.log(fav.toString());
+
+//  //SHIFT AND UNSHIFT
+
+//  let shift=chocolates.shift();// delete from start
+//  console.log(shift);
+
+//  let unshift=chocolates.unshift("Tobelerone"); //add to start
+//  console.log(unshift);
+//  console.log(chocolates);
+
+//  //SLICE AND SPLICE
+
+//  console.log(chocolates.slice(1,3)); //returns slice(piece) of an array
+
+//  console.log(chocolates.splice(2,1,"Maltestor")); //add delete and replace
+//  console.log(chocolates);
+
+//  //SPLICE
+
+//  let array=["1", "2","3","4","5"];
+
+// array.splice(2,0,100); //add element
+// console.log(array);
+
+// array.splice(2,2,100); //delete element
+// console.log(array);
+
+// array.splice(3,1,100); //Replace element
+// console.log(array);
+
+//TASK:
+
+// let companies=["Bloomberg", "Microsoft", "uber","Google","IBM","Netflix"];
+// console.log(companies);
+
+//remove first company from array
+
+// companies.shift();
+// console.log(companies);
+
+// remove uber and add ola at its place
+
+// companies.splice(2,1,"ola");
+// console.log(companies);
+
+//add amazon at the end
+
+// companies.push("amazon");
+// console.log(companies);
+
+
+
 
